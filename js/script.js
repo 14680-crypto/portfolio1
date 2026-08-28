1aad8acf3b6391f35293e8c4bca0a507673873dd
14680-crypto <14680@phanthong.ac.th>
2026-08-28 14:10:30 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,54 @@
-// ==============================
-// Portfolio JavaScript
-// ==============================
+/* =========================================
+   PORTFOLIO JAVASCRIPT
+========================================= */
 
-document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("Portfolio Website Loaded");
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    /* =====================================
+       MOBILE MENU
+    ====================================== */
+
+    const menuToggle =
+        document.getElementById("menuToggle");
+
+    const navMenu =
+        document.querySelector(".nav-menu");
+
+
+    if (menuToggle && navMenu) {
+
+        menuToggle.addEventListener(
+            "click",
+            function () {
+
+                navMenu.classList.toggle("show");
+
+            }
+        );
+
+
+        /* Close menu after clicking */
+
+        const navLinks =
+            navMenu.querySelectorAll("a");
+
+
+        navLinks.forEach(function (link) {
+
+            link.addEventListener(
+                "click",
+                function () {
+
+                    navMenu.classList.remove("show");
+
+                }
+            );
+
+        });
+
+    }
+
 
 });
